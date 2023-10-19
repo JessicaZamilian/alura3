4520c6ccc8d2b1809808573612e4f22ed843850b
JessicaZamilian <125566675+JessicaZamilian@users.noreply.github.com>
2023-10-19 11:00:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-document.querySelector('.tecla_pom')
+alert('Essa página contém virus')
+
+function tocaSomPom() {document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom') .onclick = tocaSomPom;
 document.querySelector('.tecla_clap')
 document.querySelector('.tecla_tim')
 document.querySelector('.tecla_puff')
@@ -7,3 +11,5 @@ document.querySelector('.tecla_toim')
 document.querySelector('.tecla_psh')
 document.querySelector('.tecla_tic')
 document.querySelector('.tecla_tom')
+
+
